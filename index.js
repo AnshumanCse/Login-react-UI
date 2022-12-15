15c61ff2dbbbfc0d5f0ae2833ab4ec829dde2e3e
Anshuman Tiwari <112653339+AnshumanCse@users.noreply.github.com>
2022-12-15 16:46:22 +0530

    Remove navbar

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Navbar from './Navbar;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   
     <App />
-  <Navbar/>
+ 
   <div>
   <div/>
   </React.StrictMode>
